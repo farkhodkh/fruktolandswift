6033d060f4fa10665dcb7f926a1c202e2cb671ee
Farkhod Khaknazarov <f.khaknazarov@space307.com>
2021-12-05 20:34:39 +0300

    Initial Commit

---
+++ fruktoland/fruktolandApp.swift
@@ -0,0 +1,17 @@
+//
+//  fruktolandApp.swift
+//  fruktoland
+//
+//  Created by Farkhod Khaknazarov on 05.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct fruktolandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
